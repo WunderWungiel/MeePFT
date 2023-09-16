e4b26522782b8468403a9922f7c21141312c74a0
Wunder Wungiel <dredlok706@gmail.com>
2023-09-16 23:47:40 +0200

    ...
    - Added splash and icon - thanks IarChep!!!
    - can now read config and change dir
    - Added DEB builder and control files

---
+++ build_deb.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+blue='\033[96m'
+red='\033[31m'
+reset='\033[0m'
+green='\033[32m'
+blink='\033[5m'
+yellow='\033[33m'
+cyan='\033[1;36m'
+
+set -e
+
+rm -rf tmp *.deb
+rm -rf lib/__pycache__
+
+if ! command -v dpkg-deb > /dev/null 2>&1; then
+    echo -e " $red"dpkg-dev not available..."$reset"
+    exit 0
+fi
+
+if [[ -z $1 ]] || [[ $1 == "" ]]; then
+    echo -e " $red"Provide version as argument..."$reset"
+    exit 0
+fi
+
+mkdir tmp
+cd tmp
+
+mkdir -p {DEBIAN,usr/share,opt/MeePFT/bin}
+mkdir -p usr/share/{icons/hicolor/80x80/apps,applications}
+
+cat > usr/share/applications/MeePFT.desktop <<EOF
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Type=Application
+Name=MeePFT
+Categories=System;
+Exec=/usr/bin/invoker --splash=/opt/MeePFT/splash.png --type=e /usr/bin/meego-terminal -n -e /usr/bin/aegis-exec -s -u user -l "/opt/MeePFT/bin/MeePFT"
+Icon=/usr/share/icons/hicolor/80x80/apps/MeePFT80.png
+EOF
+
+cat > opt/MeePFT/bin/MeePFT <<EOF
+#!/bin/sh
+
+cd /opt/MeePFT
+./main.py
+echo \$? > /tmp/meepft_status
+
+if [ \$(cat "/tmp/meepft_status") -ne "0" ]; then
+    echo "Please Enter to exit..."
+    read
+fi
+
+exit 0
+EOF
+
+cat > opt/MeePFT/config <<EOF
+[meepft]
+port = 8000
+dir = /home
+EOF
+
+cp ../res/icon80.png usr/share/icons/hicolor/80x80/apps/MeePFT80.png
+cp ../res/splash.png opt/MeePFT/
+
+cp ../main.py opt/MeePFT
+cp -r ../lib opt/MeePFT/
+
+package="meepft"
+version="$1"
+arch="armel"
+maintainer="Wunder Wungiel <me@wunderwungiel.pl>"
+size=$(LANG=C du -c opt usr | grep total | awk '{print $1}')
+depends="python-3.9.18-opt (= 3.9.18), viu-opt (= 1.4.0)"
+section="user/system"
+homepage="http://wunderwungiel.pl"
+description="Python server wrapper, built upon Python"
+display_name="MeePFT"
+
+icon=$(base64 ../res/icon80.png)
+icon2=""
+
+IFS=$'\n'
+
+for line in $icon; do
+    icon2+=" $line\n"
+done
+
+cat > DEBIAN/control <<EOF
+Package: $package
+Version: $version
+Architecture: $arch
+Maintainer: $maintainer
+Installed-Size: $size
+Depends: $depends
+Section: $section
+Priority: optional
+Homepage: $homepage
+Description: $description
+Aegis-Manifest: included
+Maemo-Display-Name: $display_name
+Maemo-Flags: visible
+Maemo-Icon-26:
+$icon2
+EOF
+
+rm -f ../md5
+find -type f | grep -v "./DEBIAN" > ../md5
+while read line; do if [ -f "$line" ]; then line=`echo "$line" | cut -c 3-`; md5sum "$line" >> DEBIAN/md5sums; echo S 15 com.nokia.maemo H 40 `sha1sum "$line" | cut -c -40` R `expr length "$line"` $line >> DEBIAN/digsigsums; fi; done < "../md5"
+rm -f ../md5
+
+cp ../control/* DEBIAN/
+
+cd ..
+
+filename="$package"_"$version"_"$arch".deb
+dpkg-deb -b --root-owner-group -Zgzip tmp/ $filename > /dev/null
+
+rm -rf tmp
+
+echo -e "$green"Package "$package" created. Output: "$cyan""$filename""$reset"
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-#!/opt/bin/python3.9
+#!/opt/python.3.9.18/bin/python3.9
 
 import sys
 import http.server
 import socketserver
 import socket
+import os
+import configparser
 import lib.tui as tui
+
+config_dir = "./config"
+
+config = configparser.ConfigParser()
+config.read(config_dir)
 
 blue = '\033[96m'
 red = '\033[31m'
@@ -16,25 +23,44 @@ yellow = '\033[33m'
 cyan = '\033[38;5;104m'
 cyan_background = '\033[48;5;104m'
 
-port = 8000
+port = int(config['meepft']['port'])
+dir = config['meepft']['dir']
 ip_address = '127.0.0.1'
 
 class quietServer(http.server.SimpleHTTPRequestHandler):
     def log_message(self, format, *args):
         pass
 
+def write_config_file():
+    with open(config_dir, 'w') as configfile:
+        config.write(configfile)
+
 class Options_Actions():
     def __init__(self):
-        global port
+        port = int(config['meepft']['port'])
+        dir = config['meepft']['dir']
         self.port = port
-        self.ip_address = self.get_ip()
+        self.dir = dir
+        result = self.get_ip()
+        if result == "Error":
+            self.error = True
+        else:
+            self.ip_address = result
 
     def get_ip(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+        except OSError:
+            tui.rprint(f" {red}OSError occured.{reset}")
+            tui.rprint(" Network may be unreachable. Check your Wi-Fi connection.")
+            input(f" {cyan}Press any key to Exit...{reset}")
+            sys.exit(0)
         return s.getsockname()[0]
 
     def start_server(self):
+
+        os.chdir(self.dir)
         httpd = socketserver.TCPServer(("", self.port), quietServer)
         tui.rprint(f" HTTP server working on port {cyan}'{self.port}'{reset}")
         tui.rprint(f" Visit {self.ip_address}:{self.port} in browser.")
@@ -45,7 +71,12 @@ class Options_Actions():
             tui.rprint(f" Stopping server...")
             httpd.shutdown()
             httpd.server_close()
-            tui.press_enter() 
+            tui.press_enter()
+        except OSError:
+            tui.rprint(f" {red}OSError occured.{reset}")
+            tui.rprint(f" Network may be unreachable or port in use.")
+            input(f" {cyan}Press any key to Exit...{reset}")
+            return
 
     def port_change(self):
         port = self.port
@@ -66,22 +97,65 @@ class Options_Actions():
                 break
 
         self.port = int(answer)
+        config['meepft']['port'] = str(self.port)
+        write_config_file()
         print(f" New port: {cyan}'{self.port}'{reset}.")
+        tui.press_enter()
+
+    def dir_change(self):
+        dir = self.dir
+        tui.clean()
+        while True:
+            tui.frame(text="Change directory")
+            tui.rprint(f" Current directory: {cyan}'{dir}'{reset}.")
+            tui.rprint(f" Input new directory or press Enter to keep current: ", _end='')
+
+            answer = input()
+
+            if not os.path.isdir(answer):
+                tui.rprint(f"{red} Directory doesn't exist...    {reset}", time=0.03)
+                continue
+            else:
+                break
+
+        self.dir = os.path.abspath(answer)
+        config['meepft']['dir'] = self.dir
+        write_config_file()
+        print(f" New directory: {cyan}'{self.dir}'{reset}.")
         tui.press_enter()
 
     def about(self):
 
         tui.frame(
-            f"""{cyan}MeePFT{reset}© 2023 WunderWungiel
-Version: {bold}0.0.1{reset}
+            text = f"""{cyan}MeePFT{reset}© 2023 WunderWungiel
+Version: {bold}{bold}0.0.1{reset}
 
 A simple Python HTTP wrapper
 written using Python 3.
 
 Join our Telegram group:
 
-https://t.me/linuxmobile_world""",
-            second_frame=True
+https://t.me/linuxmobile_world"""
+        )
+
+        tui.press_enter()
+        tui.clean()
+
+    def donate(self):
+
+        tui.frame(
+            text = f"""{cyan}{bold}Donating{reset}
+            
+If you want to donate for my
+small work, you can do it here:
+
+donationalerts.com/r/WunderWungiel
+
+Thank you for every 
+$, €, £, zł, etc., etc. ♥
+
+This really motivates.""",
+            second_frame=False
         )
 
         tui.press_enter()
@@ -91,22 +165,25 @@ https://t.me/linuxmobile_world""",
         sys.exit(0)
 
 def main():
-    options_actions = Options_Actions()
-
-    text = f"""Welcome to MeePFT!
-(current port: {cyan}'{port}'{reset})"""
-
-    options = {
-        "Start": options_actions.start_server,
-        "Change port": options_actions.port_change,
-        "About": options_actions.about,
-        'Exit': options_actions.exit
-    }
 
     while True:
-        result = tui.menu(text=text, options=options)
-        if result:
-            return result
+
+        options_actions = Options_Actions()
+
+        text = f"""Welcome to MeePFT!
+(current port: {cyan}'{port}'{reset})"""
+
+        options = {
+            "Start": options_actions.start_server,
+            "Change port": options_actions.port_change,
+            "Change directory": options_actions.dir_change,
+            "Donate": options_actions.donate,
+            "About": options_actions.about,
+            "Exit": options_actions.exit
+        }
+
+        while True:
+            result = tui.menu(text=text, options=options)
 
 if __name__ == "__main__":
     main()
